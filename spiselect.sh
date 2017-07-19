a152b8cc64e427b263ac6922bb850ae4105cc828
computermouth <computermouth@crunchbangplusplus.org>
2017-07-19 17:54:30 -0400

    Fixed some syntax and logic errors

--- spiselect.sh
+++ spiselect.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -x
 
-MOUNTS="$(mount | grep ${CONFIG_BASE})"
 CONFIG_BASE="/sys/kernel/config"
 TARGET_DIR="${CONFIG_BASE}/device-tree/overlays/spi"
 TARGET_DIR_DTBO="${TARGET_DIR}/dtbo"
 TARGET_DIR_STATUS="${TARGET_DIR}/dtbo"
 DTBO="/lib/firmware/nextthingco/chip/sample-spi-chippro.dtbo"
 
+MOUNTS="$(mount | grep ${CONFIG_BASE})"
+
 echo "Performing setup"
 
-if [ -z ${MOUNTS} ];then
+if [ -z "${MOUNTS}" ];then
 	mount -t configfs none ${CONFIG_BASE}
 	MOUNTS="$(mount | grep ${CONFIG_BASE})"
 	if [ -z ${MOUNTS} ];then
@@ -19,16 +20,12 @@ fi
 
 echo "Creating directory structure"
 
-if [ -d ${TARGET_DIR} ]; then 
-	echo "Error: ${TARGET_DIR} already exists" && exit 1
-else
-	mkdir -p ${TARGET_DIR}
-fi
+mkdir -p "${TARGET_DIR}"
 
 echo "moving $dtbo_location to $target_location"
 
-if [ -f ${DTBO} && -f ${TARGET_DIR_DTBO} ]; then
-	cat ${DTBO} > ${TARGET_DIR_DTBO}
+if [ -f "${DTBO}" ] && [ -f "${TARGET_DIR_DTBO}" ]; then
+	cat "${DTBO}" > "${TARGET_DIR_DTBO}"
 else
 	echo "Error: either the .dtbo or target directory is missing" && exit 1
 fi
