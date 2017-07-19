2d99bc201406679a14b3b838d28e75c3d7fa4f05
computermouth <computermouth@crunchbangplusplus.org>
2017-07-19 14:34:51 -0700

    Upload to test

--- spiselect.sh
+++ spiselect.sh
@@ -1,6 +1,38 @@
-#!/bin/sh
+#!/bin/sh -x
 
-mount -t configfs none /sys/kernel/config
-mkdir -p /sys/kernel/config/device-tree/overlays/spi
-echo "moving sample-spi-chippro.dtbo to /sys/kernel/config/device-tree/overlays/spi"
-cat /lib/firmware/nextthingco/chip/sample-spi-chippro.dtbo > /sys/kernel/config/device-tree/overlays/spi/dtbo
+MOUNTS="$(mount | grep ${CONFIG_BASE})"
+CONFIG_BASE="/sys/kernel/config"
+TARGET_DIR="${CONFIG_BASE}/device-tree/overlays/spi"
+TARGET_DIR_DTBO="${TARGET_DIR}/dtbo"
+TARGET_DIR_STATUS="${TARGET_DIR}/dtbo"
+DTBO="/lib/firmware/nextthingco/chip/sample-spi-chippro.dtbo"
+
+echo "Performing setup"
+
+if [ -z ${MOUNTS} ];then
+	mount -t configfs none ${CONFIG_BASE}
+	MOUNTS="$(mount | grep ${CONFIG_BASE})"
+	if [ -z ${MOUNTS} ];then
+		echo "Error: failed to mount configfs" && exit 1
+	fi
+fi
+
+echo "Creating directory structure"
+
+if [ -d ${TARGET_DIR} ]; then 
+	echo "Error: ${TARGET_DIR} already exists" && exit 1
+else
+	mkdir -p ${TARGET_DIR}
+fi
+
+echo "moving $dtbo_location to $target_location"
+
+if [ -f ${DTBO} && -f ${TARGET_DIR_DTBO} ]; then
+	cat ${DTBO} > ${TARGET_DIR_DTBO}
+else
+	echo "Error: either the .dtbo or target directory is missing" && exit 1
+fi
+
+if [ "$(cat ${TARGET_DIR_STATUS})" -eq "unapplied" ]; then
+	echo "Error: failed to apply device tree overlay" && exit 1
+fi
